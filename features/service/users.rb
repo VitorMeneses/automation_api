5be66c4da402265f5b85b3d25bd49e377a243908
vitor <vitorramiloks@gmail.com>
2021-05-04 02:47:57 -0300

    Criando classe para mapear requisições

---
+++ features/service/users.rb
@@ -0,0 +1,22 @@
+﻿module Users
+  class Rest
+    include HTTParty
+
+    base_uri URL[AMBIENTE]['base_hmg'] + ENDPOINT['users']
+    def get_name(params)
+      response = self.class.get("?name=#{params['name']}",
+                                headers: {
+                                    'Content-Type' => 'application/json',
+                                })
+      response
+    end
+
+    def get_id
+      response = self.class.get("/#{$id_user}/posts",
+                                headers: {
+                                    'Content-Type' => 'application/json',
+                                })
+      response
+    end
+  end
+end
